12f850a2c7ce3e3ec847369d0f3bdb00bbe71ac2
SebastiaanHag <sebastiaan.hagoort@hva.nl>
2025-03-12 02:02:30 +0100

    #6
    ik heb nu een route toegevoegd voor de gifts met een slug. werkt alleen nog niet helemaal krijg nog een get requet error.

--- server.js
+++ server.js
@@ -5,28 +5,10 @@ import express from 'express'
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from 'liquidjs';
 
-
-// console.log('Hieronder moet je waarschijnlijk nog wat veranderen')
-
-// Doe een fetch naar de data die je nodig hebt
-// const apiResponse = await fetch('...')
-
-
-// Lees van de response van die fetch het JSON object in, waar we iets mee kunnen doen
-// const apiResponseJSON = await apiResponse.json()
-
-
-
-// Controleer eventueel de data in je console
-// (Let op: dit is _niet_ de console van je browser, maar van NodeJS, in je terminal)
-// console.log(apiResponseJSON)
-
-
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
 
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
-// Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static('public'))
 
 // Stel Liquid in als 'view engine'
@@ -34,7 +16,6 @@ const engine = new Liquid();
 app.engine('liquid', engine.express()); 
 
 // Stel de map met Liquid templates in
-// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
 app.set('views', './views')
 
 // Maak een GET route voor de index (meestal doe je dit in de root, als /)
@@ -46,28 +27,44 @@ app.get('/', async function (request, response) {
    response.render('index.liquid', { data: apiResponseJSON.data });
 })
 
-app.get('/gifts', async function (request, response) {
-  const apiResponse = await fetch('https://fdnd-agency.directus.app/items/milledoni_products')
-  const apiResponseJSON = await apiResponse.json()
 
-  console.log(apiResponseJSON)
-   response.render('gifts.liquid', { data: apiResponseJSON.data });
-})
+// app.get('/gifts/', async function (request, response) {
+//   response.render('gifts.liquid')
+// })
 
-// Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
-// Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
+// app.get('/gifts/:id', async function (request, response) {
+//   const GiftResponse = await fetch(`https://fdnd-agency.directus.app/items/milledoni_products/${request.params.id}`);
+//   const GiftResponseJSON = await GiftResponse.json();
+
+//   response.render('gifts.liquid', { data: GiftResponseJSON.data });
+  
+// });
+
+
+app.get('/gifts/:slug', async function (request, response) {
+  console.log(`Fetching gift from API: https://fdnd-agency.directus.app/items/milledoni_products?${filter}`);
+  const slug = request.params.slug;
+  const filter = `&filter={"slug":"${slug}"}`;
+
+  const giftResponse = `https://fdnd-agency.directus.app/items/milledoni_products?${filter}`;
+  const giftResponseJSON = await giftResponse.json();
+
+  response.render("gifts.liquid", { data: giftResponseJSON}); // Stuur de JSON direct terug voor debugging
+});
+
+
+// post om gifts te favoriten maken
 app.post('/', async function (request, response) {
   // Je zou hier data kunnen opslaan, of veranderen, of wat je maar wilt
   // Er is nog geen afhandeling van een POST, dus stuur de bezoeker terug naar /
   response.redirect(303, '/')
 })
 
-app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!")
-})
+// 404 handler
+// app.use((req, res, next) => {
+//   res.status(404).send("Sorry can't find that!")
+// })
 
-// Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
 app.set('port', process.env.PORT || 8000)
 
 // Start Express op, haal daarbij het zojuist ingestelde poortnummer op
